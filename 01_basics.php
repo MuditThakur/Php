13059766caf4d7e15812cb44cfb2df478795e0f8
MuditThakur <muditthakur10@gmail.com>
2024-08-05 16:28:58 +0530

    first commit

---
+++ 01_basics.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+// Global variable
+$color = "red";
+echo "My car is " . $color . "<br>";
+echo "My house is " . $color . "<br>";
+echo "My boat is " . $color . "<br>";
+
+$x = 10;
+var_dump($x);
+echo "<br>";
+
+var_dump("hello");
+echo "<br>";
+
+var_dump(10.53);
+echo "<br>";
+
+var_dump(null);
+echo "<br>";
+
+var_dump([10, 20, 44]);
+echo "<br>";  // Fixed syntax error here
+?>
+
+<?php
+$name = "John";
+$age = 30;
+
+$message = "Hello, my name is $name and I am $age years old.";
+
+echo $message;
+?>
+
+<?php 
+// Local and static variable example
+function abc() {
+  static $a = 4;  // Static variable
+  echo $a . "<br>";
+  $a++;
+}
+
+abc();
+abc();
+abc();
+?>
+
+<?php
+// Global and local variable example
+$globalVar = "I am a global variable";
+
+function testGlobal() {
+  global $globalVar;  // Accessing global variable inside function
+  $localVar = "I am a local variable";
+  
+  echo $globalVar . "<br>";
+  echo $localVar . "<br>";
+}
+
+testGlobal();
+
+// Trying to access local variable outside the function will cause an error
+// Uncommenting the following line will cause an error
+// echo $localVar;
+?>
+
+</body>
+</html>
